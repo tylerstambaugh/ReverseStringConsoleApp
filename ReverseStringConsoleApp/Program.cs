4302d92fe9e620ee8b735f9a262d7dc79bda2594
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2022-06-13 20:53:47 -0400

    Add project files.

---
+++ ReverseStringConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿
+string inputString = "gorgonzola";
+
+Console.WriteLine(ReverseString.Reverse(inputString));
+
+Console.ReadLine();
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        string returnString = "";
+        Console.WriteLine($"input length {input.Length}");
+
+        for(int i = input.Length -1; i > -1; i--)
+        {
+            Console.WriteLine($"i = {i}");
+            returnString = returnString + input[i];
+            Console.WriteLine(returnString);
+        }
+
+        return returnString;
+    }
+}
